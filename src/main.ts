c83086d3a53a54ebce7fadc50b0f632965e79e51
ngimdock <ngimdockzemfack@gmail.com>
2023-02-20 14:10:32 +0100

    Add redis to the session.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { ConfigService } from '@nestjs/config';
+import { createClient } from 'redis';
+import * as connectRedis from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const logger = new Logger('bootstrap');
+
   const configService = app.get(ConfigService);
+
+  const RedisStore = connectRedis(session);
+  const redisClient = createClient({
+    url: configService.getOrThrow('REDIS_URL'),
+    legacyMode: true,
+  });
 
   app.use(
     session({
       secret: configService.getOrThrow('SESSION_SECRET'),
       resave: false,
       saveUninitialized: false,
+      store: new RedisStore({
+        client: redisClient,
+      }),
     }),
   );
 
@@ -25,6 +38,13 @@ async function bootstrap() {
       forbidUnknownValues: true,
     }),
   );
+
+  await redisClient
+    .connect()
+    .then(() => logger.debug('Redis connected.'))
+    .catch((err) => {
+      throw err;
+    });
 
   await app.listen(5000);
 }
